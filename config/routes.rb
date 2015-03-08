2291b996896fb4e4b719ef859d7655611d3472f7
Scott Gibson <sevgibson@gmail.com>
2015-03-08 16:30:39 -0400

    change generated erb to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
