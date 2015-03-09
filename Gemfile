0f45902f44ec7ff385eb768170c017ca3eb9ffb7
Scott Gibson <sevgibson@gmail.com>
2015-03-08 22:35:34 -0400

    refactor specs

--- Gemfile
+++ Gemfile
@@ -6,10 +6,13 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-#gem 'jbuilder', '~> 2.0'
 gem 'active_model_serializers'
-#gem 'rocket_pants', '~> 1.0'
 gem 'will_paginate'
+gem 'sabisu_rails', github: "IcaliaLabs/sabisu-rails"
+gem 'compass-rails'
+gem 'furatto'
+gem 'font-awesome-rails'
+gem 'simple_form'
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
@@ -50,4 +53,5 @@ group :test do
   gem 'database_cleaner'
   gem 'launchy'
   gem 'selenium-webdriver'
+  gem "shoulda-matchers"
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,4 +13,35 @@ class Api::V1::UsersController < Api::V1::ApiController
   def show
     respond_with User.find(params[:id]), serializer: UserSerializer
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, :v1, user], serializer: UserSerializer
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, :v1, user], serializer: UserSerializer
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
